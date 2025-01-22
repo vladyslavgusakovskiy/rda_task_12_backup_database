e046009435710aa0b69c43eb184e8abfbe5ff2bc
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-01-22 21:48:11 +0200

    Solution

--- task.sh
+++ task.sh
@@ -1,1 +1,12 @@
 #! /bin/bash
+
+DB_USER="backup"
+DB_PASSWORD="1111"
+DB_NAME="ShopDB"
+BACKUP_FILE="shopdb_backup.sql"
+
+mysqldump -u $DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_FILE
+mysql -u $DB_USER -p$DB_PASSWORD "ShopDBReserve" < $BACKUP_FILE
+
+mysqldump -u $DB_USER -p$DB_PASSWORD --no-create-info $DB_NAME > $BACKUP_FILE
+mysql -u $DB_USER -p$DB_PASSWORD "ShopDBDevelopment" < $BACKUP_FILE
